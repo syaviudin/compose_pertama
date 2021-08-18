a975bc670265526e7345b25cf37b33649725679d
syaviudin <msyaviudin@gmail.com>
2021-08-18 20:44:05 +0700

    chapter 2 layouts

--- app/src/main/java/com/example/latihancompose/MainActivity.kt
+++ app/src/main/java/com/example/latihancompose/MainActivity.kt
@@ -3,32 +3,58 @@ package com.example.latihancompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.latihancompose.ui.theme.LatihanComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World!")
+            MessageCard(Message("Android", "Jetpack Compose"))
 
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name : String){
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
 }
+
 @Preview
 @Composable
-fun DefaultPreview(){
-    LatihanComposeTheme {
-        MessageCard(name = "Android")
-    }
+fun DefaultPreview() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's greate!")
+    )
+
 }
 
